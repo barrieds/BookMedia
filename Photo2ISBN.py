cd85f00fd36470de9d6e62c62080f8c7144ece10
barrieds <seamus.dcd@gmail.com>
2025-07-04 20:05:34 +0100

    Code now goes through multiple combinations to diminish false results from the returned image data

--- Photo2ISBN.py
+++ Photo2ISBN.py
@@ -1,6 +1,8 @@
 import easyocr
 import cv2
 import requests
+import json
+from itertools import combinations
 
 #Test image, the actual input will be from the users camera
 bookimage = 'shiver.jpg'
@@ -14,43 +16,22 @@ reader = easyocr.Reader(['en'], gpu=False)
 #This is what reads the text from the image
 result = reader.readtext(loaded_img)
 
-#This is the function that prints the text returned from the image
-def print_text():
-    #The _'s is telling the return values to ignore that value, the values are bbox, text, confidence
-    for (_, text, _) in result:
-        print(text)
-        return text
+#The _'s is telling the return values to ignore that value, the values are bbox, text, confidence
+texts = [text for _, text, _ in result]
 
-print_text()
+queries = []
 
-bookreq = print_text()
+for size in [2,3]:
+    queries.extend([' '.join(combo) for combo in combinations(texts, size)])
 
-#requesting the query on the database website
-r = requests.get(f'https://openlibrary.org/search.json?q={bookreq}')
+for query in queries:
+    r = requests.get(f'https://openlibrary.org/search.json?q={query}')
+    data = r.json()
 
-#Printing the url and status code to check what data is returned from the query and if an error happened
-print(f"URL: {r.url} Status Code: {r.status_code}\n")
-
-#This loads the JSON from the returned data
-jsonreturn = json.loads(r.content)
-
-Title = (jsonreturn["docs"][0]["title"])
-Author = (jsonreturn["docs"][0]["author_name"])
-
-try:
-    ISBN = (jsonreturn["docs"][0]["isbn_"])
-
-except KeyError:
-    ISBN = "ISBN could not be found"
-
-#This part prints out the content found in the returned content
-if Title == "" or Author == "":
-    print("Title or Author was not found")
-
-elif ISBN == "":
-    print("ISBN number could not be found")
-
-else:
-    print(Title)
-    print(Author[-2:2])
-    print(ISBN)
+    if data["numFound"] == 1:
+        title = data["docs"][0].get("title", "Unknown Title")
+        author = data["docs"][0].get("author_name", ["Unknown Author"])[0]
+        ISBN = data["docs"][0].get("isbn_", "Unknown ISBN")
+        print(f"Book: {title} by {author}")
+        print(f"ISBN: {ISBN}")
+        break
